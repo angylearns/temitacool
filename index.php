99f9160cb89b5f5540ee129acdd030a9ac01330a
aams91 <99359053+angylearns@users.noreply.github.com>
2023-05-14 20:22:34 +0200

    A ver esto

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<h1>This is my index.</h1>
+
+<?php get_footer(); ?>
+
+
+<!-- En get_header() y get_footer(), como no especiqué nada, WordPress va a buscar unos archivos llamados "header.php" y "footer.php" en la carpeta del tema -->
